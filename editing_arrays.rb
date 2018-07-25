240343f2041aa995ede034c9779caf462526e956
clairespills <purpleprincess372@gmail.com>
2018-07-25 16:20:32 +0000

    automatically backed up by learn

--- editing_arrays.rb
+++ editing_arrays.rb
@@ -20,8 +20,8 @@ def add_colors
   rainbow_colors[0]="red"
   rainbow_colors[1]="light_red"
   rainbow_colors[2]="light_yellow"
-  rainbow_colors>>"green"
-  rainbow_colors>>"blue"
+  rainbow_colors.push"green"
+  rainbow_colors.push"blue"
   return rainbow_colors
 end
 
